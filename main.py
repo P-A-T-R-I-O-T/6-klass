78a776cb977f77db334b3ce4a38cfdea293f42ca
KonstantinKolesov <P-A-T-R-I-O-T-kolesow@yandex.ru>
2023-03-16 16:45:22 +0300

    Начал писать меню, но пока выводиться меню с файла use_function. Как так получается?

--- main.py
+++ main.py
@@ -3,3 +3,29 @@
 
 """
 from invalid_menu import invalid_menu_item
+from obraz.use_functions import top_menu, menu_selection
+from decorator import border_siporaters
+
+"""def menu():
+    text_menu = 
+    Главное меню
+    1. Один игрок портив компютера
+    2. 2 игрока
+    3. 2 компютера
+    4 Выход    
+    
+    print(text_menu)
+    item_number = input('Как вы хотите сыграть? \n Введите пункт меню')
+    return int(item_number)
+
+menu()"""
+
+text_menu = """
+    Главное меню
+    1. Один игрок портив компютера
+    2. 2 игрока
+    3. 2 компютера
+    4 Выход    
+    """
+print(text_menu)
+item_number = input('Как вы хотите сыграть? \n Введите пункт меню')
