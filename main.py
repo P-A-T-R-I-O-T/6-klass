06f0a367cc97fae8a6f5cb59f4077e9921f17c42
KonstantinKolesov <P-A-T-R-I-O-T-kolesow@yandex.ru>
2023-03-17 18:39:22 +0300

    Начал создавать класс по созданию карточки

--- main.py
+++ main.py
@@ -1,17 +1,3 @@
-"""
-ticket= 0 # билет
-card = 0 # карточка
-line = 0 # строка
-column = 0 # столб
-cell = 0 # ячейка
-object = 0 # объект
-
-ticket = card * 2 == line * 6 == column * 9 == cell * 54 == object * 30
-card = line * 3 == column * 9 == cell * 27 == object * 15
-line = column * 9 == object * 5
-column = cell * 3 == object
-"""
-
 from invalid_menu import invalid_menu_item, cleaning # Предупреждение о неправильном вводе меню и Очистка терминала
 from obraz.use_functions import menu_selection # Выделение нижнего пункта меню
 from decorator import border_siporaters # Выделение верхнего пункта меню
@@ -109,6 +95,49 @@ def first_menu():
 
         else:
             invalid_menu_item('Неверный пункт меню')
+
+
+def generate_unique_numbers(count, minimal, max):
+    objects = []
+    while len(objects) < count:
+        new = random(minimal, max)
+        if new not in objects:
+            objects.append(new)
+    return objects
+
+class Card:
+    """
+    ticket= 0 # билет
+    card = 0 # карточка
+    line = 0 # строка
+    column = 0 # столб
+    cell = 0 # ячейка
+
+    object = 0 # объект
+
+    ticket = card * 2 == line * 6 == column * 9 == cell * 54 == object * 30
+    card = line * 3 == column * 9 == cell * 27 == object * 15
+    line = column * 9 == object * 5
+    column = cell * 3 == object
+    """
+    line = 3
+    cell = 9
+    object = 5
+    data = None
+    emptynum = 0
+    crossednum = -1
+
+    def __init__(self):
+        uniques_count = self.object * self.line
+        uniques = generate_unique_numbers(uniques_count, 1, 90)
+        self.data = []
+        for i in range(0, self.line):
+            tmp = sorted(uniques[self.object * i: self.object * (i + 1)])
+            empty_nums_count = self.cell - self.object
+            for j in range(0, empty_nums_count):
+                index = random(0, len(tmp))
+                tmp.insert(index, self.emptynum)
+            self.data += tmp
 
 
 first_menu()
